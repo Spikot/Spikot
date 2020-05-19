bf9b837758df0c16b8017900ced7336e73b5dfa5
HeartPattern <heartpattern@kakao.com>
2020-05-20 02:53:40 +0900

    Add NMS ItemStack api
    Signed-off-by: HeartPattern <heartpattern@kakao.com>

---
+++ core/src/main/kotlin/kr/heartpattern/spikot/item/NMSItemStackAdapter.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2020 Spikot project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package kr.heartpattern.spikot.item
+
+import kr.heartpattern.spikot.adapter.getAdapterOf
+import kr.heartpattern.spikot.nbt.CompoundTag
+import org.bukkit.inventory.ItemStack
+
+interface NMSItemStackAdapter {
+    fun loadFromTag(tag: CompoundTag): ItemStack
+    fun saveToTag(item: ItemStack): CompoundTag
+    fun getTag(item: ItemStack): CompoundTag?
+    fun hasTag(item: ItemStack): Boolean
+    fun setTag(item: ItemStack, tag: CompoundTag?)
+    fun isCraftItemStack(item: ItemStack): Boolean
+    fun isNMSCraftItemStack(item: ItemStack): Boolean
+    fun toCraftItemStack(item: ItemStack): ItemStack
+
+    companion object Impl : NMSItemStackAdapter by getAdapterOf()
+}
